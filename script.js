c13bcc56f857337474d7425af5e26b8dc1f5cf05
Rexuly <104594084+Rexuly@users.noreply.github.com>
2025-07-29 02:49:03 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function initRipple() {
-  const btn = document.querySelector("button");
-  btn.textContent = "Verifying...";
-  setTimeout(() => {
-    btn.textContent = "You passed!";
-    btn.style.backgroundColor = "#4CAF50";
-  }, 2000);
-}
